617e0754c7cc53d42bac39e08c81c0bea5777af9
RODO <planon21@gmail.com>
2017-01-20 12:45:17 +0900

    Add file

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# EnglishRadio
-
-##ADS
-영어공부를 위한 영어라디오 앱
-
-##영어공부합시다! 
+# RadioProto
---
+++ RadioProto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadioProto
+//
+//  Created by byung-soo kwon on 2017. 1. 9..
+//  Copyright © 2017년 byung-soo kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
