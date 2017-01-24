7c8ca2b6ae3687dc12f836fe22fef5e5ae2af9f7
Kwon Byung Soo <planon21@gmail.com>
2017-01-24 12:18:39 +0900

    Add data class

--- EnglishRadio/Sources/StationData.swift
+++ EnglishRadio/Sources/StationData.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2017년 byung-soo kwon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+
+class StatoinData{
+    
+    var stationName :String
+    var stationStreamURL :String
+    var stationImageURL :String
+    var stationDesc :String
+    var stationLongDesc :String
+    
+    init(name: String, streamURL: String, imageURL: String, desc: String, longDesc: String) {
+        self.stationName      = name
+        self.stationStreamURL = streamURL
+        self.stationImageURL  = imageURL
+        self.stationDesc      = desc
+        self.stationLongDesc  = longDesc
+    }
+
+
+    
+    
+    
+    
+}
