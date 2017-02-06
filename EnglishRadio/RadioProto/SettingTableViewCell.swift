07587b7996e24c172c3cbab1e1c6e20487c1aa76
Kwon Byung Soo <planon21@gmail.com>
2017-02-07 00:30:58 +0900

    UI update

--- EnglishRadio/RadioProto/SettingTableViewCell.swift
+++ EnglishRadio/RadioProto/SettingTableViewCell.swift
@@ -10,15 +10,10 @@ import UIKit
 
 class SettingTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var channelLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+  
+    
+    
 }
--- EnglishRadio/RadioProto/SettingViewController.swift
+++ EnglishRadio/RadioProto/SettingViewController.swift
@@ -14,8 +14,11 @@ class SettingViewController: UIViewController, UITableViewDelegate,UITableViewDa
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.backgroundColor = UIColor.clear
-//        tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        
+        //테이블 뷰 백그라운드 색상 및 구분선 UI 조작
+        tableView.backgroundColor = UIColor.white.withAlphaComponent(0.1)
+        tableView.separatorColor = UIColor.white.withAlphaComponent(0.3)
+        tableView.separatorInset.left = 0
 //        // Do any additional setup after loading the view.
     }
 
@@ -25,11 +28,11 @@ class SettingViewController: UIViewController, UITableViewDelegate,UITableViewDa
     }
     
     
-    
+    //row 갯수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
-        
         return 3
+        
     }
     
     
@@ -38,23 +41,44 @@ class SettingViewController: UIViewController, UITableViewDelegate,UITableViewDa
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        //첫번째 row 에 country
+        if indexPath.row == 0 {
+            let cell:SettingTableViewCell
+            cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath) as! SettingTableViewCell
+            cell.backgroundColor = UIColor.clear
+            cell.countryLabel?.textColor = UIColor.white.withAlphaComponent(0.8)
+            
+            cell.countryLabel?.text = "Country"
+            print("section1")
+            return cell
         
+        //두번째 row에 channel
+        }else if indexPath.row == 1 {
+            let cell:SettingTableViewCell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as! SettingTableViewCell
+            cell.backgroundColor = UIColor.clear
+            cell.channelLabel?.textColor = UIColor.white.withAlphaComponent(0.8)
+            cell.channelLabel?.text = "Channel"
+            return cell
         
-        return cell
+        //세번째 row에 data
+        }else{
+            let cell:SettingTableViewCell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath) as! SettingTableViewCell
+            cell.backgroundColor = UIColor.clear
+            cell.dataLabel?.textColor = UIColor.white.withAlphaComponent(0.8)
+            
+            cell.dataLabel?.text = "3G/LTE"
+            return cell
+            
+        }
         
         
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
-
+    
 }
