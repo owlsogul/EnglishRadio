8dfd237867a6f6e1a970b35122d1602080ace14d
carolsim <carolsim.js@gmail.com>
2017-02-10 14:52:04 +0900

    즐찾 테이블에 하트누른 스테이션 추가

--- EnglishRadio/RadioProto/FavoriteViewController.swift
+++ EnglishRadio/RadioProto/FavoriteViewController.swift
@@ -14,7 +14,24 @@ class FavoriteViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var tableView: UITableView!
     
-    var stationInfoFromPrevController: StationInfo?
+    var favStationInfos: Results<StationInfo>?
+    // ViewController의 화면이 보여지기 직전에 불려질 함수
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ViewController의 화면이 보여질 예정입니다")
+        
+        // Realm을 초기화하여 realm 이라는 이름으로 사용합니다
+        let realm = try? Realm()
+        
+        // PhotoInfo에 해당하는 자료들을 데이터베이스에서 불러와서 photoInfos에 넣어줍니다
+        self.favStationInfos = realm?.objects(StationInfo.self)
+        
+        // 테이블뷰를 새로고침 합니다
+        // > 테이블뷰를 새로고침하면 테이블뷰가 보스에게 다시 row의 개수와 데이터를 요청합니다
+        self.tableView.reloadData()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,25 +47,34 @@ class FavoriteViewController: UIViewController, UITableViewDelegate, UITableView
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("보스, 1 번째 색션은 줄을 몇개를 보여줘야 하나요?)")
         
-        return 10
-        // 무엇을 보여줄지 알려주는 함수
-        
-        
-        
-        
+        // photoInfos는 옵셔널이므로 옵셔널에 값이 없으면 0을 돌려주세요
+        return self.favStationInfos?.count ?? 0
     }
     
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "favCell", for: indexPath)
         
-        return cell
+        // cell의 indexPath의 row에 해당하는 사진 정보를 가져와 봅니다
+        if let info = self.favStationInfos?[indexPath.row] {
+            
+            cell.backgroundColor = UIColor.clear
+            // 사진정보의 title을 텍스트 레이블에 세팅
+            cell.textLabel?.text = info.stationData
+            
+            /*
+            if let imageData = info.imageData {
+                cell.imageView?.image = UIImage(data: imageData, scale: 0.1)
+            }
+            */
+            
+            // cell의 detailTextLabel에 세팅
+            cell.detailTextLabel?.text = info.stationCountry
+        }
         
+        return cell
     }
-    
     
     
     //    // ADDIMAGEVIEWCONTROLLER가 화면에 보이기 직전에 수행할 일을 넣어줍니다.
