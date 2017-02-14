6ad5cc9cb2feda99e3a11a15cc0c6f06bf1cc1b1
wissenschaft89 <cauadc@CAUADC-29.local>
2017-02-14 11:43:26 +0900

    TimerSwift 다시커밋합니다.

--- EnglishRadio/TimerViewController.swift
+++ EnglishRadio/TimerViewController.swift
@@ -1,6 +1,6 @@
 //
 //  TimerViewController.swift
-//  
+//
 //
 //  Created by CAUADC on 2017. 2. 9..
 //
@@ -8,21 +8,38 @@
 
 import UIKit
 
-class TimerViewController: UIViewController,UITableViewController {
-
+class TimerViewController: UIViewController {
     
-    @IBOutlet weak var theLabel : UILabel
-    var timer = NSTimer()
+    
+    @IBOutlet weak var theLabel : UILabel!
+    var timer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
-    @IBAction func StartButton(sender: any)
- 
-   // timer = NSTimer.scheduledTimerWithTimeInterval(1.0)
+    
+    func timerResult(){
+        let theDate = NSDate()
+        theLabel.text="\(theDate)"
+        
+    }
+    
+    @IBAction func StartButton(sender: AnyObject){
+        
+      timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: Selector("timeResults"), userInfo: nil, repeats: true)
+        
+        
+        
+    }
+    
+    // timer = NSTimer.scheduledTimerWithTimeInterval(1.0)
     //https://www.youtube.com/watch?v=QKXZRRSlDss 참고
-    @IBAction func StopButton(sender: any)
+    @IBAction func StopButton(sender: AnyObject){
+    
+    timer.invalidate()
+    
+    }
     
     
     
@@ -30,7 +47,7 @@ class TimerViewController: UIViewController,UITableViewController {
     
     
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
